c6069c6d42c69bff964041fb9ec1f48dd95f47fe
0-0Rohit-Roshan <rohitroshanjena2017@gmail.com>
2022-07-26 20:49:16 +0530

    Testing files

---
+++ Language/Python/Random/Testing.py
@@ -0,0 +1,10 @@
+__version__ = 3.4
+print("--------------------------------------------------------------")
+print("|        You are about to upgrade nwg-shell to .       |")
+print("|    This will modify and/or replace your config files.      |")
+print("|                                                            |")
+print("|   If something goes wrong, run 'nwg-shell-installer -a'    |")
+print("|        or 'nwg-shell-installer' (interactive mode),        |")
+print("|            in order to install default configs.            |")
+print("--------------------------------------------------------------")
+
