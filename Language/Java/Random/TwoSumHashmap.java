a9484061527d3e265c4b89c05a075f485da83e04
0-0Rohit-Roshan <rohitroshanjena2017@gmail.com>
2022-07-27 00:00:13 +0530

    DSA practice

---
+++ Language/Java/Random/TwoSumHashmap.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int[] twoSum(int[] nums, int target) {
+    int ans[] = new int[2];
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    map.put(nums[0], 0);
+    for(int i = 1; i < nums.length; i++) {
+      int val = target - nums[i];
+      if(map.containsKey(val)) {
+        ans[0] = map.get(val);
+        ans[1] = i;
+        return ans;
+      } else {
+        map.put(nums[i], i);
+      }
+    }
+    return ans;
+  }
+}
